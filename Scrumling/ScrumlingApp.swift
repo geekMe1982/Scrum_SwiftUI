541f19bd46d99ba3f3892413acd5a2c9c268af81
Adam Khalifa <70577640+geekMe1982@users.noreply.github.com>
2024-03-15 19:35:34 +0300

    Initial Commit

---
+++ Scrumling/ScrumlingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumlingApp.swift
+//  Scrumling
+//
+//  Created by Adam Khalifa on 15.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumlingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
